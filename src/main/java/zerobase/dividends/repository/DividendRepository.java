7b94874f6beb29a3d8b5b16e2404a07f9426217d
eunsun <eunsunjj9501@gmail.com>
2024-04-03 14:13:07 +0900

    Implement scheduling function

--- src/main/java/zerobase/dividends/repository/DividendRepository.java
+++ src/main/java/zerobase/dividends/repository/DividendRepository.java
@@ -4,9 +4,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import zerobase.dividends.domain.Dividend;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
 public interface DividendRepository extends JpaRepository<Dividend, Long> {
     List<Dividend> findAllByCompanyId(Long companyId);
+
+    boolean existsByCompanyIdAndDate(Long companyId, LocalDateTime date);
 }
